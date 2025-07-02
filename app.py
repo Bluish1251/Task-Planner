66753415bec9b89c0b83aca3e2b418acfbacb1ca
Bluish1251 <Cm6of6@gmail.com>
2025-07-02 03:45:34 -0400

    Python file changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, send_from_directory
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -69,4 +70,5 @@ def delete_task(task_id):
     return jsonify({'status': 'task deleted'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=True)
